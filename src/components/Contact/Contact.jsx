38f4e8b582336d3bb678d6e54ff140db1c7e577c
victorf.y <victor.softwaredesign@gmail.com>
2024-04-18 14:38:08 -0300

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/victorfyonemoto/">
+            Linkedin
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/victorfyonemoto">Github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
